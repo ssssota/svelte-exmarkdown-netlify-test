01f3f01eff9ce09f924c28b4b0fea5dfad77edf8
TOMIKAWA Sotaro <sotaro.tomikawa@zozo.com>
2022-07-29 09:52:19 +0900

    optimize deps for build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+import { defineConfig } from 'vite';
+export default defineConfig(({ command }) => ({
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: command === 'serve' ? [] : ['remark-parse']
+	}
+}))
