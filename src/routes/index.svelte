b884cdeb24678694d519a495913ca6f6eb2e1204
ssssota <tommysouta@gmail.com>
2022-08-01 22:30:23 +0900

    use svelte-exmarkdown

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Markdown from 'svelte-exmarkdown';
+  let md = '# Hello svelte-exmarkdown';
+</script>
+
+<p>
+  <textarea bind:value={md} />
+</p>
+
+<Markdown {md} />
