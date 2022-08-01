8179523f8b47d0fc760d646091cdd7f8eb151c25
ssssota <tommysouta@gmail.com>
2022-08-01 23:18:41 +0900

    use cjs ssr time

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		format: 'cjs'
+	},
 };
 
 export default config;
